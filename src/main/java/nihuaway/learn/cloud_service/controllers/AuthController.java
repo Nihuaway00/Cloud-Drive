278d0728a2df74013e8b083f17972cd601419b13
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-28 12:49:55 +0300

    init auth register tests

---
+++ src/main/java/nihuaway/learn/cloud_service/controllers/AuthController.java
@@ -0,0 +1,10 @@
+package nihuaway.learn.cloud_service.controllers;
+
+public class AuthController {
+    public void register(){}
+
+    public void login(){}
+
+    public void logout(){}
+}
+
---
+++ src/test/java/nihuaway/learn/cloud_service/controllers/AuthControllerUnitTest.java
@@ -0,0 +1,24 @@
+package nihuaway.learn.cloud_service.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+class AuthControllerUnitTest {
+    @Mock
+    private AuthController authController;
+
+    @Test
+    void successRegistered(){
+
+    }
+
+    @Test
+    void emailAlreadyRegistered(){
+
+    }
+
+    @Test
+    void invalidPasswordOnRegister(){
+
+    }
+}
