5ceb7e7212911dc784dc494fee60cee178d8fc00
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-28 12:45:58 +0300

    #

---
+++ src/test/java/nihuaway/learn/cloud_service/TestCloudServiceApplication.java
@@ -0,0 +1,11 @@
+package nihuaway.learn.cloud_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCloudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CloudServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ src/test/java/nihuaway/learn/cloud_service/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package nihuaway.learn.cloud_service;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.oracle.OracleContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+	@Bean
+	@ServiceConnection
+	OracleContainer oracleFreeContainer() {
+		return new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"));
+	}
+
+	@Bean
+	@ServiceConnection(name = "redis")
+	GenericContainer<?> redisContainer() {
+		return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+	}
+
+}
