3e6fff12d1bda07892918e7c8eaf42bea33953a5
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-28 13:56:05 +0300

    setup postgres connection

--- src/main/java/nihuaway/learn/cloud_service/dto/UserRegisterDto.java
+++
@@ -1,7 +0,0 @@
-package nihuaway.learn.cloud_service.dto;
-
-public class UserRegisterDto {
-    private String username;
-    private String password;
-    private String nickname;
-}
--- src/test/java/nihuaway/learn/cloud_service/TestCloudServiceApplication.java
+++ src/test/java/nihuaway/learn/cloud_service/TestCloudServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestCloudServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(CloudServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(CloudServiceApplication::main).run(args);
 	}
 
 }
