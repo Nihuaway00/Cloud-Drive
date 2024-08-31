c83e62e4311a4a76abb3bc36f10d1fea4f4b3cf3
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-31 19:09:01 +0300

    init auth controller

---
+++ src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
@@ -0,0 +1,18 @@
+package nihuaway.learn.cloud_service.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+	public void register(){
+
+	}
+
+	public void login(){
+
+	}
+
+	public void logout(){
+
+	}
+}
