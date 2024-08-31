36a6da99aa73edb12af52ec1b5e1a29161c7fcb7
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-31 20:23:59 +0300

    add mapping to controller

--- src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
+++ src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
@@ -1,17 +1,20 @@
 package nihuaway.learn.cloud_service.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller("/auth")
 public class AuthController {
+
+	@RequestMapping("/register")
 	public void register(){
 
 	}
-
+	@RequestMapping("/login")
 	public void login(){
 
 	}
-
+	@RequestMapping("/logout")
 	public void logout(){
 
 	}
