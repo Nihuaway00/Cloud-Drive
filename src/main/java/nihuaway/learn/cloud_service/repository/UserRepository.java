b3703d16ea78ec82c3d4666358ef1af5c44722b1
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-31 19:11:35 +0300

    init user repository

---
+++ src/main/java/nihuaway/learn/cloud_service/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package nihuaway.learn.cloud_service.repository;
+
+import nihuaway.learn.cloud_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
