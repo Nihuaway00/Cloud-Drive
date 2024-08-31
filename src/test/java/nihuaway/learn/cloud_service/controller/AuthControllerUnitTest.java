5f55bd823f50725de536eb254e4158d1b5bf1be3
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-31 19:10:35 +0300

    init auth controller test

---
+++ src/test/java/nihuaway/learn/cloud_service/controller/AuthControllerUnitTest.java
@@ -0,0 +1,12 @@
+package nihuaway.learn.cloud_service.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerUnitTest {
+	@Test
+	void successRegister(){
+
+	}
+}
