e29536d4a26239073b44fa91a9e493da8b9848af
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-31 20:11:34 +0300

    init UserService for Spring Security

---
+++ src/main/java/nihuaway/learn/cloud_service/service/UserService.java
@@ -0,0 +1,22 @@
+package nihuaway.learn.cloud_service.service;
+
+import nihuaway.learn.cloud_service.entity.User;
+import nihuaway.learn.cloud_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (user == null) throw new UsernameNotFoundException("User not found");
+		return (UserDetails) user;
+	}
+}
