05f2cc1938427f202a8bde8e6578817ee7055fdc
Nihuaway00 <nikitaemaeny@gmail.com>
2024-08-31 20:14:55 +0300

    add UserService to security config

--- src/main/java/nihuaway/learn/cloud_service/config/SecurityConfig.java
+++ src/main/java/nihuaway/learn/cloud_service/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package nihuaway.learn.cloud_service.config;
 
+import nihuaway.learn.cloud_service.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +27,7 @@ public class SecurityConfig {
 						.loginPage("/login")
 						.permitAll()
 				)
+				.userDetailsService(userDetailsService())
 				.rememberMe(Customizer.withDefaults());
 
 		return http.build();
@@ -33,5 +36,10 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserService();
 	}
 }
