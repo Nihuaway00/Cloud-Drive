2d0041ab6a8ad8a345f1464cd5b1dc72ac31a1fc
Nihuaway00 <nikitaemaeny@gmail.com>
2024-09-01 11:06:39 +0300

    update security config. add jwt filter

--- src/main/java/nihuaway/learn/cloud_service/config/SecurityConfig.java
+++ src/main/java/nihuaway/learn/cloud_service/config/SecurityConfig.java
@@ -1,15 +1,16 @@
 package nihuaway.learn.cloud_service.config;
 
-import nihuaway.learn.cloud_service.service.UserService;
+import nihuaway.learn.cloud_service.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,29 +18,24 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(authorize -> authorize
-						.requestMatchers("/**").permitAll()
-						.anyRequest()
-						.authenticated()
+						.requestMatchers("/auth/**").permitAll()
+						.anyRequest().authenticated()
 				)
-				.formLogin(formLogin -> formLogin
-						.loginPage("/login")
-						.permitAll()
-				)
-				.userDetailsService(userDetailsService())
-				.rememberMe(Customizer.withDefaults());
-
-		return http.build();
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.build();
 	}
 
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService() {
-		return new UserService();
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter();
 	}
 }
