30e8e997b9b75aee4fb294094bbbfa6986d9a05c
Nihuaway00 <nikitaemaeny@gmail.com>
2024-09-02 10:34:11 +0300

    add validation to UserRegisterDto

--- src/main/java/nihuaway/learn/cloud_service/dto/UserRegisterDto.java
+++ src/main/java/nihuaway/learn/cloud_service/dto/UserRegisterDto.java
@@ -1,5 +1,8 @@
 package nihuaway.learn.cloud_service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +13,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegisterDto {
+	@NotEmpty
+	@Email(message = "Некорректный формат почты")
 	private String username;
+
+	@NotEmpty
+	@Pattern(
+		regexp = "^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{6,16}$",
+		message = "Пароль должен быть от 6 до 16 символов, содержать хотя бы одну цифру и одну заглавную букву, и состоять только из латинских букв и цифр"
+	)
 	private String password;
 }
