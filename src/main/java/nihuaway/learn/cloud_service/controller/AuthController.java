f1927771f5cc872cc4badee33d293942dfc0359b
Nihuaway00 <nikitaemaeny@gmail.com>
2024-09-02 10:36:27 +0300

    done register

--- src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
+++ src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
@@ -1,20 +1,52 @@
 package nihuaway.learn.cloud_service.controller;
 
-import org.springframework.stereotype.Controller;
+import jakarta.validation.Valid;
+import nihuaway.learn.cloud_service.dto.UserRegisterDto;
+import nihuaway.learn.cloud_service.entity.User;
+import nihuaway.learn.cloud_service.repository.UserRepository;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/auth")
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+	private UserRepository userRepository;
 
-	@RequestMapping("/register")
-	public void register(){
-
+	@Autowired
+	public AuthController(UserRepository userRepository) {
+		this.userRepository = userRepository;
 	}
-	@RequestMapping("/login")
+
+	@PostMapping("/register")
+	public ResponseEntity<?> register(@RequestBody @Valid UserRegisterDto dto){
+		try {
+			if (userRepository.findByUsername(dto.getUsername()).isPresent()) {
+				return new ResponseEntity<>(
+						new BadRequestException("Такой пользователь уже зарегестрирован"),
+						HttpStatus.CONFLICT
+				);
+			}
+
+			User user = new User();
+			user.setUsername(dto.getUsername());
+			user.setPassword(dto.getPassword());
+			userRepository.save(user);
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			return new ResponseEntity<>(e, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@PostMapping("/login")
 	public void login(){
 
 	}
-	@RequestMapping("/logout")
+	@PostMapping("/logout")
 	public void logout(){
 
 	}
