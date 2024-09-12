5b52a9f2381f64ba4091f96bc2fb71d4243bfda7
Nihuaway00 <nikitaemaeny@gmail.com>
2024-09-12 08:50:32 +0300

    add access/refresh token to response

--- src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
+++ src/main/java/nihuaway/learn/cloud_service/controller/AuthController.java
@@ -1,10 +1,13 @@
 package nihuaway.learn.cloud_service.controller;
 
+import io.jsonwebtoken.Jwts;
 import jakarta.validation.Valid;
+import nihuaway.learn.cloud_service.dto.JwtResponse;
 import nihuaway.learn.cloud_service.dto.UserLoginDto;
 import nihuaway.learn.cloud_service.dto.UserRegisterDto;
 import nihuaway.learn.cloud_service.entity.User;
 import nihuaway.learn.cloud_service.repository.UserRepository;
+import nihuaway.learn.cloud_service.util.JwtUtil;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +17,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -48,14 +55,25 @@ public class AuthController {
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody UserLoginDto dto) {
 		Optional<User> user = userRepository.findByUsername(dto.getUsername());
-		if(user.isEmpty()) {
+		if (user.isEmpty()) {
 			return new ResponseEntity<>("Такой пользователь не существует", HttpStatus.BAD_REQUEST);
 		}
-		if(!user.get().getPassword().equals(dto.getPassword())){
+		if (!user.get().getPassword().equals(dto.getPassword())) {
 			return new ResponseEntity<>("Неверный пароль", HttpStatus.BAD_REQUEST);
 		}
 
-		return ResponseEntity.ok().build();
+		JwtResponse jwtResponse = new JwtResponse();
+
+		Map<String, String> data = new HashMap<>();
+		data.put("username", user.get().getUsername());
+		data.put("id", user.get().getId().toString());
+
+		String accessToken = JwtUtil.createToken(data, JwtUtil.getAccessTokenExpiration());
+		String refreshToken = JwtUtil.createToken(data, JwtUtil.getRefreshTokenExpiration());
+
+		jwtResponse.setAccessToken(accessToken);
+		jwtResponse.setRefreshToken(refreshToken);
+		return ResponseEntity.ok().body(jwtResponse);
 	}
 
 	@PostMapping("/logout")
