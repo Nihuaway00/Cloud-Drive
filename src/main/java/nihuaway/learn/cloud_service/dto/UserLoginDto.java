918995e9f18f6accd4c215f8aaac0b27706806e3
Nihuaway00 <nikitaemaeny@gmail.com>
2024-09-12 07:35:12 +0300

    init UserLoginDto

---
+++ src/main/java/nihuaway/learn/cloud_service/dto/UserLoginDto.java
@@ -0,0 +1,27 @@
+package nihuaway.learn.cloud_service.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDto {
+	@NotEmpty
+	@Email(message = "Некорректный формат почты")
+	private String username;
+
+	@NotEmpty
+	@Pattern(
+		regexp = "^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{6,16}$",
+		message = "Пароль должен быть от 6 до 16 символов, содержать хотя бы одну цифру и одну заглавную букву, и состоять только из латинских букв и цифр"
+	)
+	private String password;
+}
